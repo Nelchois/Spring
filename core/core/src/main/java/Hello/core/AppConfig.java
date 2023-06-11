eab7f6592a3ee7f4ba8b72ef34342880fdfed3bc
Nelchois <reiethd@gmail.com>
2023-06-11 23:53:43 +0900

    Feat: make project
    refactoring AppConfig.java
    split memberService(), orderService()
    
    Done

--- core/core/src/main/java/Hello/core/AppConfig.java
+++ core/core/src/main/java/Hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package Hello.core;
 
+import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
@@ -10,11 +11,19 @@ import Hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository() , discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
